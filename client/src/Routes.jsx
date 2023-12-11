d427a58218b8f9be970aed20c817f29f43d464bd
Pratham <ppratham812@gmail.com>
2023-12-11 21:58:47 +0530

    Chat Page Developed

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,11 +1,12 @@
 import RegisterLogin from "./RegisterLogin.jsx";
 import {useContext} from "react";
 import {UserContext} from "./UserContext.jsx";
+import ChatPage from "./ChatPage.jsx";
 
 export default function Routes(){
     const {username,id} = useContext(UserContext);
     if(username){
-        return `logged in as ${username}`;
+        return <ChatPage/>;
     }
 
     return(
