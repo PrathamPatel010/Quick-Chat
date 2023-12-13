a35e98f7b383fa2541164d1017ed2b0fb5ad94f2
Pratham <ppratham812@gmail.com>
2023-12-13 22:49:31 +0530

    Chat Page Modified & Message Model Created

---
+++ api/model/Message.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    sender:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    recipient:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    text:{
+        type:String,
+    },
+},{timestamps:true});
+
+const Message = mongoose.model('Message',messageSchema);
+
+module.exports = Message;
