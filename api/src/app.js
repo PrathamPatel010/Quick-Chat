5987058f8fcffe79c861099d9c2b29a9182fc7b7
Pratham Patel <ppratham812@gmail.com>
2024-06-10 15:49:26 +0530

    backend initialized, prisma setup done,schema developed

---
+++ api/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const colors = require('colors');
+const {PORT} = require('./config/serverConfig');
+
+const setupAndStartServer = async () => {
+    const app = express();
+    app.use(express.json());
+    app.use(express.urlencoded({extended:true}));
+    app.listen(PORT,()=>{
+        console.log(`[LOG] : CollegeConnect Backend is listening on port ${PORT}`.green.bold);
+    });
+    app.get('/',(req, res)=>res.send(`<h1>CollegeConnect Backend is up and running</h1>`));
+}
+
+setupAndStartServer();
